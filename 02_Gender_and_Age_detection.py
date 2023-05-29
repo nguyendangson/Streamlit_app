3167ba1f19f1ecdd83b244325d2831d42e032b60
Son Nguyen <33495366+nguyendangson@users.noreply.github.com>
2023-05-29 17:26:42 -0400

    Update 02_Gender_and_Age_detection.py

--- 02_Gender_and_Age_detection.py
+++ 02_Gender_and_Age_detection.py
@@ -132,6 +132,9 @@ if picture:
     "Waiting seconds..."
     show_test(picture)
     #st.image("https://drive.google.com/file/d/1eDNB8HWWDEBVCBLqp_j8fhCaNMie3nq1/view?usp=share_link", width=300)
+    
+    
+st.markdown(f"""<a href="https://info.flagcounter.com/xaga"><img src="https://s01.flagcounter.com/count2/xaga/bg_FFFFFF/txt_000000/border_CCCCCC/columns_3/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>""", unsafe_allow_html=True)
 
     
         
