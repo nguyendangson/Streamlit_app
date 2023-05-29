ecd7a160325c50fc6b8e69e77e32f025df0404c5
Son Nguyen <33495366+nguyendangson@users.noreply.github.com>
2023-05-29 17:25:53 -0400

    Update 10_Data_Map.py

--- 10_Data_Map.py
+++ 10_Data_Map.py
@@ -13,3 +13,6 @@ starbuck.columns = ['lon', 'lat']
 
 st.write(starbuck)
 st.map(starbuck)
+
+
+st.markdown(f"""<a href="https://info.flagcounter.com/xaga"><img src="https://s01.flagcounter.com/count2/xaga/bg_FFFFFF/txt_000000/border_CCCCCC/columns_3/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>""", unsafe_allow_html=True)
