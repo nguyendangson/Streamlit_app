931935bbf78e470872da54e8165778011a8999e1
Son Nguyen <33495366+nguyendangson@users.noreply.github.com>
2023-05-29 17:43:59 -0400

    Update 06_Stocks.py

--- 06_Stocks.py
+++ 06_Stocks.py
@@ -99,7 +99,7 @@ if Input_stock:
         #st.metric(label="Stock price", value=stock_info.get_live_price(Input_stock))
         
         
-   st.markdown(f"""<a href="https://info.flagcounter.com/xaga"><img src="https://s01.flagcounter.com/count2/xaga/bg_FFFFFF/txt_000000/border_CCCCCC/columns_3/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>""", unsafe_allow_html=True)
+st.markdown(f"""<a href="https://info.flagcounter.com/xaga"><img src="https://s01.flagcounter.com/count2/xaga/bg_FFFFFF/txt_000000/border_CCCCCC/columns_3/maxflags_20/viewers_0/labels_1/pageviews_1/flags_0/percent_0/" alt="Flag Counter" border="0"></a>""", unsafe_allow_html=True)
 
         
 
