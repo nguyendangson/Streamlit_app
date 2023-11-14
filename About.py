ac1fd0dd8693cbb9deb92ca5040e7bddbb12e945
Son Nguyen <33495366+nguyendangson@users.noreply.github.com>
2023-11-14 16:17:54 -0500

    Update About.py

--- About.py
+++ About.py
@@ -17,11 +17,11 @@ show_pages([
     Page("04_Contact.py", "Contact me", "📪"),
     #Section(name="Math", icon="🔢"),
     Section(name="Data Science", icon="🤖"),
-    #Page("06_Stocks.py", "Stocks", "📈"),
+    Page("06_Stocks.py", "Stocks", "📈"),
     Page("02_Gender_and_Age_detection.py", "Detection", "👁️"),
     Page("10_Data_Map.py", "Data Map", "🌍"),
     Page("13_Natural_Language_Processing.py", "Natural Language Processing", "🔠"),
-    #Page("14_Music_and_Songs_Generated.py", "Music and Songs Generated", "♬♪"),
+    Page("14_Music_and_Songs_Generated.py", "Music and Songs Generated", "♬♪"),
     
     #Section(name="Information Security", icon="🔐"),
     #Page("05_Hacking.py", "Hack", "😈")
