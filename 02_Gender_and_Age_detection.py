7557b47c664003f3d68ed9c9da60dcbe3364dc44
Son Nguyen <33495366+nguyendangson@users.noreply.github.com>
2023-11-14 23:34:00 -0500

    Update 02_Gender_and_Age_detection.py

--- 02_Gender_and_Age_detection.py
+++ 02_Gender_and_Age_detection.py
@@ -3,7 +3,7 @@ from PIL import Image
 import numpy as np
 
 import deepface
-import cv2
+#import cv2
 
 from deepface import DeepFace
 from st_pages import Page, Section, add_page_title, show_pages ,show_pages_from_config
