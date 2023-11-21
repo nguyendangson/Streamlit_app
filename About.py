2f337058e9b155c05facbff230bee13933cc949c
Son Nguyen <33495366+nguyendangson@users.noreply.github.com>
2023-11-20 21:45:14 -0500

    Update About.py

--- About.py
+++ About.py
@@ -21,7 +21,7 @@ show_pages([
     Page("02_Gender_and_Age_detection.py", "Detection", "👁️"),
     Page("10_Data_Map.py", "Data Map", "🌍"),
     #Page("13_Natural_Language_Processing.py", "Natural Language Processing", "🔠"),
-    Page("14_Music_and_Songs_Generated.py", "Music and Songs Generated", "♬♪"),
+    Page("14_Music_and_Songs_Generated.py", "Music of Math", "♬♪"),
     
     #Section(name="Information Security", icon="🔐"),
     #Page("05_Hacking.py", "Hack", "😈")
